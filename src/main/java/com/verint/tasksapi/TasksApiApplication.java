a49cc1f6c34b1952f9d84c0cd56723ba78eeb2d7
James Brown <James.Brown@verint.com>
2022-06-08 17:32:13 +0100

    Add OpenAPI processor and local db connection

--- src/main/java/com/verint/tasksapi/TasksApiApplication.java
+++ src/main/java/com/verint/tasksapi/TasksApiApplication.java
@@ -2,8 +2,10 @@ package com.verint.tasksapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class TasksApiApplication {
 
     public static void main(String[] args) {
