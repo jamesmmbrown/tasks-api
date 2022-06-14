3e266cbbb9b58b84fbb6aa47cad48409bb95ff8c
James Brown <James.Brown@verint.com>
2022-06-14 09:48:03 +0100

    Add Sonarqube and test coverage

---
+++ src/test/java/com/verint/tasksapi/TaskDTOMatcher.java
@@ -0,0 +1,49 @@
+package com.verint.tasksapi;
+
+import com.verint.tasksapi.model.TaskDTO;
+import lombok.RequiredArgsConstructor;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.util.Objects;
+
+@RequiredArgsConstructor
+public class TaskDTOMatcher extends TypeSafeMatcher<TaskDTO>  {
+
+    private final Long id;
+    private final String name;
+    private final Boolean status;
+
+    public static TaskDTOMatcher task(Long id, String name, Boolean status) {
+        return new TaskDTOMatcher(id, name, status);
+    }
+
+    @Override
+    protected boolean matchesSafely(TaskDTO item) {
+        return Objects.equals(item.getId(), id) &&
+                item.getName().equals(name) &&
+                item.getStatus().equals(status);
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText(" ");
+        describeTask(description, id, name, status);
+    }
+
+    @Override
+    protected void describeMismatchSafely(TaskDTO item, Description mismatchDescription) {
+        mismatchDescription.appendText("was ");
+        describeTask(mismatchDescription, item.getId(), item.getName(), item.getStatus());
+    }
+
+    private void describeTask(Description description, Long id, String name, Boolean status) {
+        description.appendText("<TaskDTO(id=")
+                .appendValue(id)
+                .appendText(", name= ")
+                .appendValue(name)
+                .appendText(", status= ")
+                .appendValue(status)
+                .appendText(")>");
+    }
+}
--- src/test/java/com/verint/tasksapi/TasksServiceTest.java
+++ src/test/java/com/verint/tasksapi/TasksServiceTest.java
@@ -1,6 +1,39 @@
 package com.verint.tasksapi;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.verint.tasksapi.model.TaskDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static com.verint.tasksapi.TaskDTOMatcher.task;
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.mapstruct.factory.Mappers.getMapper;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class TasksServiceTest {
 
+    @Mock
+    private TaskRepository taskRepository;
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        taskService = new TaskService(taskRepository, getMapper(TaskMapper.class));
+    }
+
+    @Test
+    void getAll_shouldReturnAllTasks() {
+        when(taskRepository.findAll()).thenReturn(asList(new Task(1L, "task 1", false), new Task(2L, "task 2", true)));
+
+        List<TaskDTO> allTasks = taskService.getAll();
+
+        assertThat(allTasks, contains(task(1L, "task 1", false), task(2L, "task 2", true)));
+    }
 }
