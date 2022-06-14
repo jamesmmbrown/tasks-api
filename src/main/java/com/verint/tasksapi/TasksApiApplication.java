1270f75f577d878ddd7126b327d07c21a029f474
James Brown <James.Brown@verint.com>
2022-06-14 17:09:00 +0100

    Add CORS configuration

--- src/main/java/com/verint/tasksapi/TasksApiApplication.java
+++ src/main/java/com/verint/tasksapi/TasksApiApplication.java
@@ -2,9 +2,11 @@ package com.verint.tasksapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class TasksApiApplication {
 
---
+++ src/main/java/com/verint/tasksapi/configuration/CORSConfiguration.java
@@ -0,0 +1,23 @@
+package com.verint.tasksapi.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+
+    private final String[] allowedOriginPatterns;
+
+    public CORSConfiguration(@Value("${if.cors.allowed.origins}") String[] allowedOriginPatterns) {
+        this.allowedOriginPatterns = allowedOriginPatterns;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns(allowedOriginPatterns);
+    }
+}
